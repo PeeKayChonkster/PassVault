01af8252f6bbfe0e2c9db5f8ac8fda5f4a1d90bc
LEVERX\pavel.klimenka <pavel.klimenka@leverx.com>
2022-05-04 17:51:10 +0300

    Implement add, list and remove commands

--- PassVault/Program.cs
+++ PassVault/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PassVault.Interfaces;
 using PassVault.Services;
+using PassVault.Models;
 
 internal static class Program
 {
@@ -7,12 +8,16 @@ internal static class Program
     {
         "help - list all commands",
         "clear - clear terminal window",
+        "add (short id) (password) [(desctiprion)] - add new password",
+        "list - list all passwords",
+        "remove (short id) - remove password form database",
         "exit - close program"
     };
 
     private static IInputOutput io = new ConsoleIO();
     private static bool _exiting = false;
     private static readonly char _starter = '>';
+    private static Database db = new Database();
 
     public static void Main(string[] args)
     {
@@ -29,6 +34,8 @@ internal static class Program
                 ExecCmd(words[0], words[1..^0]);
             }
         }
+
+        db.Dispose();
     }
 
     private static void ExecCmd(string cmd, params string[] args)
@@ -50,9 +57,39 @@ internal static class Program
                     io.Clear();
                     break;
                 }
+            case "add":
+                {
+                    if (args.Length == 0 || args[0] == "" || args[1] == "") return;
+                    string desc = args.Length > 2? args[2] : "";
+                    db.Add(new Password(args[0], args[1], desc));
+                    io.WriteLine("Added Password " + args[0]);
+                    break;
+                }
+            case "remove":
+                {
+                    if (args.Length == 0 || args[0] == "") return;
+                    if(db.Delete(args[0]))
+                    {
+                        io.WriteLine("Deleted password " + args[0]);
+                    }
+                    else
+                    {
+                        io.WriteLine("Didn't find password " + args[0]);
+                    }
+                    break;
+                }
+            case "list":
+                {
+                    foreach(Password password in db.Passwords)
+                    {
+                        io.WriteLine(password.ToString());
+                    }
+                    break;
+                }
             case "exit":
                 {
                     _exiting = true;
+                    io.WriteLine("Goodbye!");
                     break;
                 }
             default:
