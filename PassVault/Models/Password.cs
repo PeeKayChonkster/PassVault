9de9356f9e76bad6f2d789d02d8e05aee3dc78a8
LEVERX\pavel.klimenka <pavel.klimenka@leverx.com>
2022-05-04 16:00:26 +0300

    Add password record

---
+++ PassVault/Models/Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassVault.Models
+{
+    internal record Password
+    {
+        public string Id { get; set; }
+        public string Pass { get; set; }
+        public string Description { get; set; }
+
+        public Password(string id, string pass, string description = "")
+        {
+            this.Id = id;
+            this.Pass = pass;
+            this.Description = description;
+        }
+    }
+}
