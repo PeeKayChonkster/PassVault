e2440931e7a8eff3a7725eaf28d356fabc1ee6f6
LEVERX\pavel.klimenka <pavel.klimenka@leverx.com>
2022-05-04 16:30:58 +0300

    Implement basic cmd execution functionality

--- PassVault/Program.cs
+++ PassVault/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PassVault.Interfaces;
+using PassVault.Services;
+
+internal static class Program
+{
+    private static readonly string[] _commandsInfo =
+    {
+        "help - list all commands",
+        "clear - clear terminal window",
+        "exit - close program"
+    };
+
+    private static IInputOutput io = new ConsoleIO();
+    private static bool _exiting = false;
+    private static readonly char _starter = '>';
+
+    public static void Main(string[] args)
+    {
+        io.WriteLine("Welcome to PassVault.");
+        io.WriteLine("Your password is safe here.");
+
+        while(!_exiting)
+        {
+            io.Write(_starter);
+            string? cmd = io.ReadLine();
+            if(cmd != null)
+            {
+                string[] words = cmd.Split(' ');
+                ExecCmd(words[0], words[1..^0]);
+            }
+        }
+    }
+
+    private static void ExecCmd(string cmd, params string[] args)
+    {
+        cmd = cmd.ToLower();
+        Array.ForEach(args, (arg) => { arg = arg.ToLower(); });
+        switch(cmd)
+        {
+            case "help":
+                {
+                    foreach(string command in _commandsInfo)
+                    {
+                        io.WriteLine(command);
+                    }
+                    break;
+                }
+            case "clear":
+                {
+                    io.Clear();
+                    break;
+                }
+            case "exit":
+                {
+                    _exiting = true;
+                    break;
+                }
+            default:
+                {
+                    io.WriteLine("Enter \"help\" to list all available commands");
+                    break;
+                }
+        }
+    }
+}
