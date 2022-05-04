9ea7c53e216eb0a0b9dd98159e0b57f48e07521e
LEVERX\pavel.klimenka <pavel.klimenka@leverx.com>
2022-05-04 16:13:05 +0300

    Add Write overload to IInputOutput

--- PassVault/Interfaces/IInputOutput.cs
+++ PassVault/Interfaces/IInputOutput.cs
@@ -4,6 +4,7 @@ namespace PassVault.Interfaces
     internal interface IInputOutput
     {
         void Write(string line);
+        void Write(char ch);
         void WriteLine(string line);
         string? ReadLine();
         void Clear();
