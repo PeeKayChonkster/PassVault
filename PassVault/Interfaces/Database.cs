977083daa0d2ec74f351d0aff8baa2e7b276940d
LEVERX\pavel.klimenka <pavel.klimenka@leverx.com>
2022-05-04 16:07:15 +0300

    Add IDatabase interface

---
+++ PassVault/Interfaces/Database.cs
@@ -0,0 +1,11 @@
+﻿using PassVault.Models;
+
+namespace PassVault.Interfaces
+{
+    internal interface Database
+    {
+        List<Password> Passwords { get; }
+        void Add(Password password);
+        void Delete(string id);
+    }
+}
---
+++ PassVault/Interfaces/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿using PassVault.Models;
+
+namespace PassVault.Interfaces
+{
+    internal interface IDatabase
+    {
+        List<Password> Passwords { get; }
+        void Add(Password password);
+        void Delete(string id);
+    }
+}
